f22b7abcf593e66057d070d9365024b06fce29e6
Hlopa <bariolga@yandex.ru>
2020-08-01 16:56:38 +0300

    add products.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,10 @@
 $(function(){
-    
-    var mixer = mixitup('.products__inner-box');
+    var isMain = Boolean(document.getElementById('main'));
 
+    if (isMain) {
+      var mixer = mixitup('.products__inner-box');
+    }
+  
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -14,4 +17,32 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4
     }); 
+
+    
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+  });
+
+
+  $(".icon-th-list").on("click", function(){
+      $(".product-page__items > .product__item").addClass('list');
+      $(".icon-th-list").addClass('active');
+      $(".icon-th-large").removeClass('active');
+  });
+
+  $(".icon-th-large").on("click", function(){
+    $(".product-page__items > .product__item").removeClass('list');
+    $(".icon-th-large").addClass('active');
+    $(".icon-th-list").removeClass('active');
+});
+
+  
+
+
+  
 });
